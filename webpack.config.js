0bc2a90feb3bab586c7a2277aa1744c1f27788fd
Jackson Dean <jcdean@linkedin.com>
2017-11-27 08:24:05 -0800

    Add css/scss support and redux/react-redux dependenices

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,20 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader', // compiles Sass to CSS
+          },
+        ],
+      },
     ],
   },
 
